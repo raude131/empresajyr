207d17b17cb38325513e479d7b3151f52c8293f0
Rolando Bardales <rolando_bardales@usmp.pe>
2020-08-07 17:50:45 -0600

    Nuevos cambios

---
+++ js/validacionsolicitudpedido.js
@@ -0,0 +1,31 @@
+function validarSolicitudPedido(form){
+
+    let validacion = true;
+    const mensaje = form.mensaje;
+
+    if(mensaje.value.length === 0 || isEmpty(mensaje.value)){
+        setErrorFor(mensaje,'Ingrese un mensaje');
+        validacion = false;
+    }else{
+        setSuccessFor(mensaje);
+    }
+
+    return validacion;
+
+}
+
+function setErrorFor(input, message) {
+    const campo = input.parentElement
+    const small = campo.querySelector('small')
+    campo.className = 'campo error'
+    small.innerText = message
+}
+
+function setSuccessFor(input) {
+    const campo = input.parentElement
+    campo.className = 'campo success'
+}
+
+function isEmpty(texto){
+    return /^\s+$/.test(texto);
+}
